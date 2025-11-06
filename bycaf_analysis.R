e5814773bc901925cc21b27a743d82cf732d0799
Elsy Perez <elsy.perez@ub.edu.bz>
2025-11-06 12:01:00 -0600

    Calculate frequency of top 5 common creek name responses

--- bycaf_analysis.R
+++ bycaf_analysis.R
@@ -5,4 +5,15 @@ library("tidyverse")
 library("readxl")
 
 ## Attach data -------------------
-read_excel("bycaf_data.xlsx")
+bycaf_data<- read_excel("bycaf_data.xlsx")
+
+# Frequency of Creek Name --------------------------------
+
+df_creek_name <- bycaf_data %>%
+  mutate(Creek_Name_B1a = str_split(Creek_Name_B1a, ", ")) %>%
+  unnest(Creek_Name_B1a) %>%
+  group_by(Creek_Name_B1a) %>%
+  summarize(Total = n()) %>%
+  arrange(-Total)
+
+head(df_creek_name, 5)
